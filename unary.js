eabdafe748c95c3400a6d8591f84688030a2c530
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-23 16:43:22 +0600

    function scope in var

--- unary.js
+++ unary.js
@@ -12,7 +12,23 @@ let callGreet=greet('Jobayer')
 
 console.log(callGreet);
 
-const adding=()
 
-const composition=(...fns)=> console.log(fns);
+
+
+
+
+
+
+const add5 = x => x + 5;
+const multiply3 = x => x * 3;
+const square = x => x * x;
+
+
+// Function composition
+const compose = (...fns) => console.log(fns);
+
+// const compose = (...fns) => x => fns.reduceRight((acc, fn) => fn(acc), x);
+
+const transform = compose(square, multiply3, add5);
+// console.log(transform(2)); // ((2 + 5) * 3) ^ 2 = 441
 
---
+++ var_function.js
@@ -0,0 +1,12 @@
+function varFunc() {
+    var a=78
+}
+
+if (true) {
+    var b='golap'
+}
+console.log(a);
+
+console.log(b);
+
+
