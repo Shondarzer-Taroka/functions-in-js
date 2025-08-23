99678026456ad6d9a3391e01079d8f0947ca7a22
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-23 14:45:30 +0600

    function initializes

---
+++ unary.js
@@ -0,0 +1,15 @@
+function dream(dreamName) {
+    return dreamName
+}
+
+
+console.log(dream('being rich man '));
+
+
+const greet=(name)=> ` hello ${name}`
+
+let callGreet=greet('Jobayer')
+
+console.log(callGreet);
+
+const composition=(...fns)=>
