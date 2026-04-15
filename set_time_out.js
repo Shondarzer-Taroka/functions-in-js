68252d9d2ba3c7d176fb54d0e7e24ac4406a401f
MD NAZMUL HASAN <80097373+Shondarzer-Taroka@users.noreply.github.com>
2026-04-15 12:07:06 +0600

    Add dakoAmay function with setTimeout logging
    Added a function 'dakoAmay' with a setTimeout call that logs a message.
    
    Function add with correct way, and declared

--- set_time_out.js
+++ set_time_out.js
@@ -1,4 +1,18 @@
 
+
+console.log("Start");
+
+setTimeout(() => {
+  console.log("Executed after 2 seconds");
+}, 2000);
+
+console.log("End");
+
+
+
+
+
+
 function dakoAmay (){
   setTimeOut(){
     console.log("kmn acho tumi")
